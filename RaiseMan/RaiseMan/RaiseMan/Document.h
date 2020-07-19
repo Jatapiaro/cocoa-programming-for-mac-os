3169f57cb40cc31c8bef3b94b7732f9cb24a44c0
Jacobo Tapia <jacob.et.cetera@gmail.com>
2020-07-19 15:08:19 -0700

    Raise Man checkpoint. Chapter 8 correct

---
+++ RaiseMan/RaiseMan/RaiseMan/Document.h
@@ -0,0 +1,16 @@
+//
+//  Document.h
+//  RaiseMan
+//
+//  Created by Jacobo Tapia on 19/07/20.
+//  Copyright © 2020 Jacobo Tapia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@property (nonatomic, copy) NSMutableArray *employees;
+
+@end
+
