cb32e3aec015c6c298e26a847f6f76fb90817630
Jacobo Tapia <jacob.et.cetera@gmail.com>
2021-06-01 03:37:44 -0700

    Chapter 34

---
+++ Chapter34_Concurrency/Scattered/Scattered/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Scattered
+//
+//  Created by Jacobo Tapia on 5/30/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Chapter34_Concurrency/Scattered/Scattered/ScatteredController.h
@@ -0,0 +1,16 @@
+//
+//  ScatteredController.h
+//  Scattered
+//
+//  Created by Jacobo Tapia on 5/30/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScatteredController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
